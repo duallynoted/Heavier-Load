9920fd64a081adae2e80cef019d08d1dce16520f
Daniel Ridley <ridleydan31@gmail.com>
2018-10-22 16:24:37 -0500

    successful server side POST route for custom_exercises via Postman

--- database.sql
+++ database.sql
@@ -1,5 +1,53 @@
-CREATE TABLE person (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR (80) UNIQUE NOT NULL,
-    password VARCHAR (1000) NOT NULL
+--create database called "heavier_load"
+
+--create table which handles login id and profile information
+CREATE TABLE "person" (
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL,
+    "first_name" VARCHAR(60),
+    "last_name" VARCHAR(60),
+    "height" VARCHAR(16),
+    "weight" INT,
+    "gender" VARCHAR(40),
+    "goal" VARCHAR(300
+ );
+
+--create table to hold member-generated exercises to track weight-load over time
+CREATE TABLE "custom_exercise" (
+	"id" SERIAL PRIMARY KEY,
+	"title" VARCHAR(120),
+	"weight_load" INT,
+	"day" VARCHAR (120),
+	"person_id" INT REFERENCES "person"
 );
+
+--create table to hold member-generated measurements to track progress
+CREATE TABLE "measurement" (
+	"id" SERIAL PRIMARY KEY,
+	"body_area" VARCHAR(120),
+	"measurement" INT,
+	"person_id" INT REFERENCES "person"
+	);
+
+--query that updates(put)profile information	
+UPDATE "person" 
+SET "first_name"='Daniel',"last_name"='Ridley',"height"=73, "weight"=239,"gender"='male',"goal"='Get stronger, trimmer, and more awesomer.'
+WHERE "id" = 1;
+
+UPDATE "person" 
+SET "first_name"='Hiram',"last_name"='Willick',"height"=68, "weight"=250,"gender"='male',"goal"='Lose weight.'
+WHERE "id" = 2;
+
+--query that will insert member-generated data into custom_exercise table
+INSERT INTO "custom_exercise" ("title","weight_load","day","person_id")
+VALUES ('Bicep Curl',25,'Mondays',1);
+
+INSERT INTO "measurement" ("body_area", "measurement","person_id")
+VALUES('left arm',17,1);
+
+--query that selects first name (or whatever you want by ."x")
+SELECT "person"."first_name"
+FROM "person"
+WHERE "id" = 1;
+ 
--- server/routes/member-router.js
+++ server/routes/member-router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+//this query will make a call to the server to get member information
 router.get('/', (req, res) => {
     pool.query(`SELECT * FROM "person"
     ORDER BY "id";`)
@@ -9,17 +10,26 @@ router.get('/', (req, res) => {
             res.send(results.rows);
         }).catch((error) => {   
             res.sendStatus(500);
-            console.log('error', error);
+            console.log('error getting member', error);
         });//end GET pool query
 });//end GET call server side
 
-/**
- * POST route template
- */
+//this query will make post calls from member-generated data, creating exercises to track weight-load over time
 router.post('/', (req, res) => {
+    const newExercise= req.body;
+    const queryValues = [newExercise.title, newExercise.weight_load, newExercise.day, newExercise.person_id];
+    pool.query(`INSERT INTO "custom_exercise" ("title","weight_load","day","person_id")
+    VALUES ($1,$2,$3,$4);`, queryValues)
+        .then((results) => {
+            console.log('HEEEEEEEYYYY', results);
+            res.send(results.rows);            
+        }).catch((error) => {
+            console.log('Error POSTING exercise to PostgreSQL', error);
+            res.sendStatus(500);
+        })//end POST pool query
+});//end POST call server side
 
-});
-
+//this query will update a member's profile information	
 router.put('/:id', (req, res) => {
     let id = req.params.id;
     let body = req.body;
@@ -30,9 +40,9 @@ router.put('/:id', (req, res) => {
     .then(() => {
         res.sendStatus(200);
     }).catch(error => {
-        console.log('error updating person', error);
+        console.log('error updating member', error);
         res.sendStatus(500);
-    });//end PUT pool query
+    });//end PUT pool query 
 });//end PUT call server side
 
 
