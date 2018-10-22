8b917e0ee0bd10ced2e1d11ff1979024049f1537
Daniel Ridley <ridleydan31@gmail.com>
2018-10-22 18:04:54 -0500

    make small branding changes

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,15 +1,10 @@
 import React from 'react';
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
-
 const AboutPage = () => (
   <div>
     <div>
       <p>
-        This about page is for anyone to read!
+      This will be filled with information about how Heavier Load works.
       </p>
     </div>
   </div>
