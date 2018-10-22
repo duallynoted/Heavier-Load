68961a7446cbf5f97badc13357362fd63fab2d8d
Daniel Ridley <ridleydan31@gmail.com>
2018-10-22 15:54:59 -0500

    successful server side PUT route via Postman

--- server/routes/member-router.js
+++ server/routes/member-router.js
@@ -25,8 +25,8 @@ router.put('/:id', (req, res) => {
     let body = req.body;
     console.log(body);      
     pool.query(`UPDATE "person" 
-    SET "first_name"=$1,"last_name"=$2,"height"=$3,"weight"=$4,"gender"=$5,"goal"=$5
-    WHERE "id" = $6;`, [body.first_name, body.last_name, body.height, body.weight, body.gender, id])
+    SET "first_name"=$1,"last_name"=$2,"height"=$3,"weight"=$4,"gender"=$5,"goal"=$6
+    WHERE "id" = $7;`, [body.first_name, body.last_name, body.height, body.weight, body.gender, body.goal, id])
     .then(() => {
         res.sendStatus(200);
     }).catch(error => {
